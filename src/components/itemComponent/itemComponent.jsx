49d8a5b313d0fd19669f3506c9efa5d21b3731e9
Franco Sanchez Alejandro <francosanche@gmail.com>
2023-04-05 01:12:50 -0300

    deleting some unnecessary things

--- src/components/itemComponent/itemComponent.jsx
+++ src/components/itemComponent/itemComponent.jsx
@@ -6,14 +6,10 @@ import { Link } from 'react-router-dom'
 
 function ItemComponent({ id, img_source, alt = '', title = 'UNDEFINED',  }) {
 
-    let siteSource = process.env.PUBLIC_URL + img_source
-    console.log(img_source,"img_source")
-    console.log(siteSource,"siteSource")
-
 
     return (
         <div className='item_container'>
-                <img src={siteSource} alt={alt} />
+                <img src={img_source} alt={alt} />
             <div className='text_container'>
                 <h2>{title}</h2>
                 <Link to={`/item/${id}`} >
